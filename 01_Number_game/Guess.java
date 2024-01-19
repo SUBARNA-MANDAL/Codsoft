31ad92920fa72e9b1e23c09035899d1ef43a884f
Subarna Mandal <subarnamandal17@gmail.com>
2024-01-19 23:30:23 +0530

    ~By &ubarna

--- 01_Number_game/Guess.java
+++ 01_Number_game/Guess.java
@@ -4,7 +4,10 @@ public class Guess {
       int totattempt=0,correct=0;
       String k;
       Scanner sc = new Scanner(System.in);
-      System.out.println("Welcome to guess game!!!\nGuess a number between 0 - 100 within 5 attempts!!");
+      System.out.print("\n\t|-----------------------------------------|");
+      System.out.print("\n\t|<<<<<<<   Welcome to guess game   >>>>>>>|\n");
+      System.out.println("\t|-----------------------------------------|");
+      System.out.println("\nGuess the number between 0 - 100 within 5 attempts!!");
       do{
          int count=0,totlim=5,guessnum;
          int randomnum = (int)(Math.random()*100) + 1;
@@ -12,7 +15,7 @@ public class Guess {
          while(totlim>0){
             count++;
             totlim--;
-            System.out.print("Guess a number -->> ");
+            System.out.print("* Guess the number -->> ");
             while(true){
                if(!sc.hasNextInt()){
                   String str = sc.next();
@@ -21,28 +24,30 @@ public class Guess {
                else{
                   guessnum = sc.nextInt();
                   if(guessnum>=1 && guessnum<=100) break;
-                  else  System.out.print("Invalid input!!\n\nEnter between 1 - 100 -->> ");
+                  else  System.out.print("Invalid input!!\n\n* Enter between 1 - 100 -->> ");
                }  
             }
             if(randomnum==guessnum){
                correct++;
-               System.out.println("Hurrah !!! Correct Answer.");
-               System.out.println("You took "+count+" moves to guess the number.");
+               System.out.println("\n\t>>\t Hurrah !!! Correct Guess.\t<<");
+               System.out.print("\t>> You took "+count+" moves to guess the number.<<");
                break;
             }
-            else if(randomnum>guessnum)   System.out.print("\nYour entered number is too low.\n");
-            else  System.out.print("\nYour entered number is too high.\n");
+            else if(randomnum>guessnum)   System.out.println("\nYour entered number is too ->'low'.");
+            else  System.out.println("\nYour entered number is too ->'high'.");
 
             if(totlim>0){
-               System.out.println(totlim+" moves left to you.");
-               System.out.println("Try again.\n");
+               
+               System.out.println(totlim+" moves left to you.\n");
             }
             else{
                System.out.println("\nBetter luck next time.");
             }
          }
-         System.out.println("\n      *** You scored "+correct+" out of "+totattempt+". ***");
-         System.out.print("\nIf u want to play again type y or Y else anything -->> ");
+         System.out.println("\n\t|-----------------------------------------|");
+         System.out.println("\t\t   You scored "+correct+" out of "+totattempt+".");
+         System.out.println("\t|-----------------------------------------|");
+         System.out.print("\n* If u want to play again type y or Y else anything -->> ");
          k = sc.next();
       }while(k.toLowerCase().charAt(0)== 'y');
       sc.close();
